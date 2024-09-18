a40521611b30776ab4effd6563a750b05375099e
Terry Stifler Ramírez <terryman42@gmail.com>
2024-09-18 12:51:52 -0600

    Se agregaron los botones principales

--- Tienda_Parker/formPrincipal.Designer.cs
+++ Tienda_Parker/formPrincipal.Designer.cs
@@ -33,6 +33,11 @@ namespace Tienda_Parker
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
             this.btnInicio = new DevExpress.XtraEditors.SimpleButton();
             this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.btnUsuarios = new DevExpress.XtraEditors.SimpleButton();
+            this.btnEmpleados = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.panelControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
@@ -40,6 +45,11 @@ namespace Tienda_Parker
             // 
             // panelControl1
             // 
+            this.panelControl1.Controls.Add(this.simpleButton3);
+            this.panelControl1.Controls.Add(this.simpleButton2);
+            this.panelControl1.Controls.Add(this.simpleButton1);
+            this.panelControl1.Controls.Add(this.btnEmpleados);
+            this.panelControl1.Controls.Add(this.btnUsuarios);
             this.panelControl1.Controls.Add(this.btnInicio);
             this.panelControl1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelControl1.Location = new System.Drawing.Point(0, 0);
@@ -49,10 +59,12 @@ namespace Tienda_Parker
             // 
             // btnInicio
             // 
+            this.btnInicio.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnInicio.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnInicio.ImageOptions.Image")));
-            this.btnInicio.Location = new System.Drawing.Point(0, 0);
+            this.btnInicio.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnInicio.Location = new System.Drawing.Point(2, 2);
             this.btnInicio.Name = "btnInicio";
-            this.btnInicio.Size = new System.Drawing.Size(129, 66);
+            this.btnInicio.Size = new System.Drawing.Size(125, 66);
             this.btnInicio.TabIndex = 0;
             this.btnInicio.Text = "Inicio";
             // 
@@ -63,6 +75,61 @@ namespace Tienda_Parker
             this.panelControl2.Name = "panelControl2";
             this.panelControl2.Size = new System.Drawing.Size(671, 66);
             this.panelControl2.TabIndex = 1;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUsuarios.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image2")));
+            this.btnUsuarios.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnUsuarios.Location = new System.Drawing.Point(2, 68);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(125, 66);
+            this.btnUsuarios.TabIndex = 0;
+            this.btnUsuarios.Text = "Gestion de Usuarios";
+            // 
+            // btnEmpleados
+            // 
+            this.btnEmpleados.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmpleados.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image1")));
+            this.btnEmpleados.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnEmpleados.Location = new System.Drawing.Point(2, 134);
+            this.btnEmpleados.Name = "btnEmpleados";
+            this.btnEmpleados.Size = new System.Drawing.Size(125, 66);
+            this.btnEmpleados.TabIndex = 1;
+            this.btnEmpleados.Text = "Gestion de Empleados";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.simpleButton1.Location = new System.Drawing.Point(2, 200);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(125, 66);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Gestion de Productos";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.simpleButton2.Location = new System.Drawing.Point(2, 266);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(125, 66);
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "Gestion de Proveedores";
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.simpleButton3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.simpleButton3.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.simpleButton3.Location = new System.Drawing.Point(2, 332);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(125, 66);
+            this.simpleButton3.TabIndex = 4;
+            this.simpleButton3.Text = "Gestion de Facturas";
             // 
             // formPrincipal
             // 
@@ -86,5 +153,10 @@ namespace Tienda_Parker
         private DevExpress.XtraEditors.PanelControl panelControl1;
         private DevExpress.XtraEditors.SimpleButton btnInicio;
         private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.SimpleButton btnUsuarios;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton btnEmpleados;
     }
 }
